abb49bf671bfc428e783b55f3d1d40e9b85fb983
Ufuk Cam <ufukcam07@gmail.com>
2020-09-14 00:13:07 +0300

    contact page fix

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -2,6 +2,7 @@
   import Header from '../components/Header.svelte';
 
   export let segment;
+
 </script>
 
 <style>
--- src/routes/contact.svelte
+++ src/routes/contact.svelte
@@ -1,40 +1,52 @@
-<style>
-  figure {
-    float: left;
-    margin: -3em -7em 2em 2em;
-    max-width: 500px;
-  }
-
-  figure img {
-    border-radius: 8px;
-  }
-
-  @media (max-width: 1020px) {
-    figure {
-      float: none;
-      margin: 0 auto 2em;
-    }
-  }
-</style>
-
 <script>
-  let appName = "Ufuk Çam"
+    import Icon from 'svelte-awesome/components/Icon.svelte'
+    import { github } from 'svelte-awesome/icons'
+    import { twitter } from 'svelte-awesome/icons'
+    import { linkedin } from 'svelte-awesome/icons'
+
 </script>
 
+
+<style>
+    figure {
+        float: left;
+        margin: -3em -7em 2em 2em;
+        max-width: 500px;
+    }
+
+    figure img {
+        border-radius: 8px;
+    }
+
+    @media (max-width: 1020px) {
+        figure {
+            float: none;
+            margin: 0 auto 2em;
+        }
+    }
+
+
+</style>
+
+
+
 <svelte:head>
-  <title>Contact - {appName}</title>
+    <title>Contact</title>
 </svelte:head>
 
 <div class="container">
-  <h1>Get in touch</h1>
+    <h1>Get in touch test</h1>
 
-  <div class="col-md-8">
-    iletişim bilgileri buraya
-  </div>
 
-  <!-- <figure>
-    <img src='adam-solomon-WHUDOzd5IYU-unsplash.jpg' alt='Image of a vintage typewriter.'>
-    <figcaption>Photo by <a href="https://unsplash.com/@solomac" target="_blank">Adam Solomon</a> on Unsplash</figcaption>
-  </figure> -->
+        <a href="https://github.com/ufukcam" target="_blank"> <Icon data={github} scale="2" /> </a>
+        <a href="https://twitter.com/ufukcam" target="_blank"><Icon data={twitter} scale="2" /></a>
+        <a href="https://www.linkedin.com/in/ufukcam/" target="_blank"><Icon data={linkedin} scale="2" /></a>
+
 
 </div>
+
+    <!-- <figure>
+      <img src='adam-solomon-WHUDOzd5IYU-unsplash.jpg' alt='Image of a vintage typewriter.'>
+      <figcaption>Photo by <a href="https://unsplash.com/@solomac" target="_blank">Adam Solomon</a> on Unsplash</figcaption>
+    </figure> -->
+
